496d384a1cf2e055cc7deb5f6859de8762055e07
Mahamudul Hasan <mahamud.7info@gmail.com>
2026-07-26 21:45:19 +0600

    Add Two Sum solution in LeetCode/Easy
    Implement brute force solution for Two Sum problem.

---
+++ LeetCode/Easy/001_Two_Sum.cpp
@@ -0,0 +1,22 @@
+// Problem: Two Sum
+// Platform: LeetCode
+// Difficulty: Easy
+// Topic: Array
+// Approach: Brute Force
+// Time Complexity: O(n^2)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
+            }
+        }
+
+        return {};
+    }
+};
