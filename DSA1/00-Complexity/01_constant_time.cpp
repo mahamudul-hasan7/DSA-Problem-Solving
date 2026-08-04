c464e2ecbef828393dd46aa2e00f4a8a22491492
Mahamudul Hasan <mahamud.7info@gmail.com>
2026-08-04 23:26:55 +0600

    Add constant time example

---
+++ DSA1/00-Complexity/01_constant_time.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> numbers = {10, 20, 30, 40, 50};
+
+    // Accessing one element takes constant time
+    cout << "First element: " << numbers[0] << endl;
+
+    return 0;
+}
