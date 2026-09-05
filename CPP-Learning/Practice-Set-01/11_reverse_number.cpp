ca8a1341dca7692f77f6cbb55899a6b12627b38c
Mahamudul Hasan <mahamud.7info@gmail.com>
2026-09-05 20:31:40 +0600

    Add 11_reverse_number.cpp

---
+++ CPP-Learning/Practice-Set-01/11_reverse_number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    int rev = 0, rem;
+    while (n > 0) {
+        rem = n % 10;
+        rev = rev * 10 + rem;
+        n /= 10;
+    }
+
+    cout << "Reversed Number: " << rev << endl;
+    return 0;
+}
