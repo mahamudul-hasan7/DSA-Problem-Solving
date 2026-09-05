05d7c3b5e17a596b2be6b4c73c50d0807759b053
Mahamudul Hasan <mahamud.7info@gmail.com>
2026-09-05 20:23:57 +0600

    Remove scripts directory

--- scripts/push_now.ps1
+++
@@ -1,6 +0,0 @@
-Set-Location "D:\DSA-Problem-Solving"
-git pull --rebase origin main
-if ($LASTEXITCODE -ne 0) {
-    git pull --rebase origin master
-}
-git push origin HEAD
--- scripts/schedule_daily_push.ps1
+++
@@ -1,9 +0,0 @@
-$repo = "D:\DSA-Problem-Solving"
-$times = "09:00","13:00","17:00","21:00"
-
-foreach ($time in $times) {
-    $taskName = "DSA-Push-$($time.Replace(':',''))"
-    schtasks /Create /TN $taskName /SC DAILY /ST $time /TR "powershell -ExecutionPolicy Bypass -File \"$repo\scripts\push_now.ps1\"" /F | Out-Null
-}
-
-Write-Host "Daily push schedule created: 09:00, 13:00, 17:00, 21:00"
