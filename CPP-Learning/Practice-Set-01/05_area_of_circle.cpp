7b290a33ccb3d646eb4c8a1e38b35044a36b773d
Mahamudul Hasan <mahamud.7info@gmail.com>
2026-09-05 20:21:59 +0600

    Add 05_area_of_circle.cpp

---
+++ CPP-Learning/Practice-Set-01/05_area_of_circle.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    const double PI = 3.14159;
+    double r;
+    cout << "Enter radius: ";
+    cin >> r;
+
+    cout << "Area: " << PI * r * r << endl;
+    return 0;
+}
