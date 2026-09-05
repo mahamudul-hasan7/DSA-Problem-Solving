f1e7b91ffc690b3d0c608e8066fc5b30e5cda242
Mahamudul Hasan <mahamud.7info@gmail.com>
2026-09-06 04:11:50 +0600

    Add 14_second_largest.cpp

---
+++ CPP-Learning/Practice-Set-01/14_second_largest.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size: ";
+    cin >> n;
+
+    int arr[100];
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++) cin >> arr[i];
+
+    int largest = INT_MIN, second = INT_MIN;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > largest) {
+            second = largest;
+            largest = arr[i];
+        } else if (arr[i] > second && arr[i] != largest) {
+            second = arr[i];
+        }
+    }
+
+    cout << "Second largest: " << second << endl;
+    return 0;
+}
